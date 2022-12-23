41bdf18ca431d3767b529dea37e022548c404e61
Alabere-coder <alabere4real247@gmail.com>
2022-12-23 06:37:39 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class",
+    content: [
+        './public/**/*.html',
+        './public/**/*.js'
+    ],
+    theme: {
+        extend: {
+            spacing: {
+                "big": "48rem"
+            }
+        },
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+        },
+        fontFamily: {
+            nunito: ['Nunito', 'sans-serif']
+        }
+    },
+    plugins: [],
+}
